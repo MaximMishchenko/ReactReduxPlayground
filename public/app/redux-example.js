01286354c7487cd6cd37da41c0e547cb4848518b
MaximMishchenko <maxim.mishenko@yandex.ru>
2017-04-16 18:37:11 +0300

    Break up reducer into multible reducers

--- public/app/redux-example.js
+++ public/app/redux-example.js
@@ -9,7 +9,7 @@ let stateDefault = {
 let nextHobbyId = 1
 let nextMovieId = 1
 
-let reducer = (state = stateDefault, action) => {
+let oldReducer = (state = stateDefault, action) => {
 	console.log('new action', action)
 
 	switch (action.type) {
@@ -55,6 +55,56 @@ let reducer = (state = stateDefault, action) => {
 			return state
 	}
 }
+
+let nameReducer = (state =  'Anonymous', action) => {
+	switch (action.type) {
+		case 'CHANGE_NAME':
+			return action.name
+		default:
+			return state
+	}
+}
+
+let hobbiesReducer = (state = [], action) => {
+	switch(action.type){
+		case 'ADD_HOBBIE':
+			return [
+				...state,
+				{
+					id: nextHobbyId++,
+					hobby: action.hobby
+				}
+			]
+		case 'REMOVE_HOBBY':
+			return state.filter((hobby) => hobby.id !== action.id)
+		default:
+			return state
+	}
+}
+
+let moviesReducer = (state = [], action) => {
+	switch(action.type){
+		case 'ADD_MOVIE':
+			return[
+				...state,
+				{
+					id: nextMovieId++,
+					title: action.title,
+					genre: action.genre
+				}
+			]
+		case 'REMOVE_MOVIE':
+			return state.filter((movie) => movie.id !== action.id)
+		default:
+			return state
+	}
+}
+
+let reducer = redux.combineReducers({
+	name: nameReducer,
+	hobbies: hobbiesReducer,
+	movies: moviesReducer
+})
 
 let store = redux.createStore(reducer, redux.compose(
 	window.devToolsExtension ? window.devToolsExtension() : f => f
